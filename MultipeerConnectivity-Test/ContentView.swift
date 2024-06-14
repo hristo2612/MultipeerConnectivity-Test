aaa309384bf51a5764039e0dbc60a3b51a9a2f28
hristo2612 <hristoapps@gmail.com>
2024-06-14 14:48:34 +0300

    Initial Commit

---
+++ MultipeerConnectivity-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultipeerConnectivity-Test
+//
+//  Created by Hristo on 14.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultipeerConnectivity-Test/MultipeerConnectivity_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerConnectivity_TestApp.swift
+//  MultipeerConnectivity-Test
+//
+//  Created by Hristo on 14.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerConnectivity_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
